4de0916b1a15633ebe635425bcc7a5b9bde0719d
Avikumar <aviiiprajapati16@gmail.com>
2026-02-11 12:09:27 +0530

    first commit

---
+++ src/main/java/org/avi1606/uberprojectreviewservices/UberProjectReviewServicesApplication.java
@@ -0,0 +1,13 @@
+package org.avi1606.uberprojectreviewservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberProjectReviewServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberProjectReviewServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/avi1606/uberprojectreviewservices/UberProjectReviewServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.avi1606.uberprojectreviewservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectReviewServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
